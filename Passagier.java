932dbae447133f2109c5b3f281546ece8e30a6ab
Lars Pfeiffer <lars@pfeifferonline.de>
2026-07-08 21:42:30 +0200

    test

--- Passagier.java
+++ Passagier.java
@@ -3,10 +3,18 @@
  */
 public class Passagier {
 
+   
     private String passagierId;
     private String name;
     private String email; 
 
+
+    /**
+     * Konstruktor für die Klasse Passagier. 
+     * @param passagierId
+     * @param name
+     * @param email
+     */
     // Konstruktor
     public Passagier(String passagierId, String name, String email) {
         this.passagierId = passagierId;
@@ -15,25 +23,52 @@ public class Passagier {
     }
 
     // Getter 
+
+    /**
+    *Gibt die Passagier-ID zurück.
+    *
+    * @return die Passagier-ID
+     */
     public String getPassagierId() {
         return passagierId;
-    }   
+    } 
+    
+    
+
+     /**
+    *Gibt den Namen des Passagiers zurück.
+    *
+    * @return der Name des Passagiers
+     */
 
     public String getName() { 
         return name;
     }
 
-
+     /**
+    *Gibt die E-Mail-Adresse des Passagiers zurück.
+    *
+    * @return die E-Mail-Adresse des Passagiers
+     */
     public String getEmail() {
         return email;
     }
 
 
+    /**
+     * Setzt die E-Mail-Adresse des Passagiers.
+     * @param email
+     */
     // Setter-Methoden
     public void setEmail(String email) {
         this.email = email;
     }
     
+
+    /**
+     * Gibt eine String-Darstellung des Passagiers zurück.
+     * @return eine String-Darstellung des Passagiers
+     */
     public String toString() {
         return "Passagier ID: " + passagierId + ", Name: " + name + ", Email: " + email;
     }
