49f8c7aac8c2b20ae9d18a9bfef23305b8fe481c
Lars-8bit <146458845+Lars-8bit@users.noreply.github.com>
2026-07-19 15:35:52 +0200

    Feature/add baggage 0007 (#8)
    * commit add Gepäckinformationen
    
    * commit add Gepäckinformationen :)
    
    * chore: code clean-up (0007)
    
    ---------
    
    Co-authored-by: lars pfeiffer <Lars@pfeifferonline.de>
    Co-authored-by: Kevin Braun <69989043+Kevinpptx@users.noreply.github.com>

---
+++ GepaeckInformation.java
@@ -0,0 +1,60 @@
+/**
+ * Repräsentiert Informationen über Gepäck in einem Flugzeug.
+ * Gepäck besteht aus einer Anzahl von Koffern, die ein Passagier mit sich führt.
+ */
+public class GepaeckInformation {
+
+    /** Die Anzahl der Koffer, die ein Passagier mit sich führt. */
+    private int anzahlKoffer;
+
+    /** Die Der Preis eines Koffers. Wert 40 ist ein fiktiver Standardwert. */
+    double gebuehrProKoffer = 40.0;
+
+    /**
+     * Konstruktor für die Klasse GepäckInformation.
+     *
+     * @param anzahlKoffer die Anzahl der Koffer
+     */
+    public GepaeckInformation(int anzahlKoffer) {
+        this.anzahlKoffer = anzahlKoffer;
+    }
+
+    /**
+     * Gibt die Anzahl der Koffer zurück.
+     *
+     * @return die Anzahl der Koffer
+     */
+    public int getAnzahlKoffer() {
+        return anzahlKoffer;  
+    }
+
+    /**
+     * Setzt die Anzahl der Koffer.
+     *
+     * @param anzahlKoffer die neue Anzahl der Koffer
+     */
+    public void setAnzahlKoffer(int anzahlKoffer) {
+        this.anzahlKoffer = anzahlKoffer;
+    }
+
+    /**
+     * Berechnet die Gepäckgebühr basierend auf der Anzahl der Koffer.
+     *
+     * @return die berechnete Gepäckgebühr
+     */
+    public double berechneGepaeckgebuehr() {
+            return anzahlKoffer * gebuehrProKoffer;
+    }   
+
+    /**
+     * Gibt eine String-Darstellung der GepäckInformation zurück.
+     *
+     * @return eine String-Darstellung der GepäckInformation
+     */
+    @Override
+    public String toString() {
+        return "GepäckInformation: " +
+                "anzahlKoffer= " + anzahlKoffer;
+    }
+
+}
