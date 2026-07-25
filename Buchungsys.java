1d553ae0340e1ad174ed8eb2b3ae0997c17cc1f3
Cedric <cedi.beckmann@gmail.com>
2026-07-25 16:33:58 +0200

    Merge remote-tracking branch 'origin/main' into feature/add-UI-classes-0011

---
+++ Buchungsys.java
@@ -0,0 +1,13 @@
+
+import java.io.Serializable;
+
+public class Buchungsys implements Serializable {
+
+    public Buchungsys() {
+
+    }
+
+    public void print() {
+        System.out.println("MOOOOOIN ich bin wieder da!!!");
+    }
+}
