e6e6928de26812d70965033cee07ced177684b14
cedbe6 <beckmann.cedric@icloud.com>
2026-08-08 21:26:31 +0200

    Feature/add baggage change (#47)
    * feat: implement feature: change gepaeckinformation technical
    
    * feat: add UI
    
    * fix: issues with umbuchungsgebühr
    
    * fix: return
    
    * fix: add .toUpperCase for easier use of Flug anlegen
    
    ---------
    
    Co-authored-by: Cedric Beckmann <cedric.beckmann@deutschebahn.com>

--- Buchung.java
+++ Buchung.java
@@ -22,6 +22,7 @@ public class Buchung implements Serializable {
     private GepaeckInformation gepaeckInformation;
     private Buchungsstatus buchungsstatus;
     private double gezahlterPreis;
+    private double gezahlteUmbuchungsgebuehr = 0.0;
 
     // Beispielwerte
     private final double umbuchungsGebühr = 100.00;
@@ -163,14 +164,28 @@ public class Buchung implements Serializable {
         return gepaeckInformation;
     }
 
+    public void setAnzahlKoffer(int anzahlKoffer) {
+        this.gepaeckInformation.setAnzahlKoffer(anzahlKoffer);
+        this.gezahlterPreis = berechneGezahltenPreis();
+    }
+
     public double getGezahlterPreis() {
         return gezahlterPreis;
     }
 
-    public void setGezahlterPreis(double preis) {
-        this.gezahlterPreis = preis;
+    public void aktualisiereGezahltenPreis() {
+        this.gezahlterPreis = berechneGezahltenPreis();
     }
 
+    public double getGezahlteUmbuchungsgebuehr() {
+        return gezahlteUmbuchungsgebuehr;
+    }
+
+    public void setGezahlteUmbuchungsgebuehr(double gebuehr) {
+        this.gezahlteUmbuchungsgebuehr = gebuehr;
+    }
+
+    
 
     /**
      * Gibt eine textuelle Beschreibung der Buchung zurück.
@@ -192,6 +207,8 @@ public class Buchung implements Serializable {
                 this.gepaeckInformation.toString() +
                 "\n---------------------------------------------\n" +
                 "Damit beträgt die Buchungssumme: " +
-                String.format("%.2f", this.gezahlterPreis) + "€";
+                String.format("%.2f", this.gezahlterPreis) + "€" + 
+                "\nGezahlter Umbuchungsbetrag: " 
+                + String.format("%.2f", this.gezahlteUmbuchungsgebuehr) + "€";
     }
 }
