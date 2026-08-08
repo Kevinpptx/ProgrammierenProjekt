92c9d009a9779b8b70236e2e295d543691a53927
Cedric Beckmann <cedric.beckmann@deutschebahn.com>
2026-08-08 20:03:08 +0200

    fix: issues with umbuchungsgebühr

--- Buchung.java
+++ Buchung.java
@@ -22,6 +22,7 @@ public class Buchung implements Serializable {
     private GepaeckInformation gepaeckInformation;
     private Buchungsstatus buchungsstatus;
     private double gezahlterPreis;
+    private double gezahlteUmbuchungsgebuehr = 0.0;
 
     // Beispielwerte
     private final double umbuchungsGebühr = 100.00;
@@ -172,10 +173,19 @@ public class Buchung implements Serializable {
         return gezahlterPreis;
     }
 
-    public void setGezahlterPreis(double preis) {
-        this.gezahlterPreis = preis;
+    public void aktualisiereGezahltenPreis() {
+        this.gezahlterPreis = berechneGezahltenPreis();
     }
 
+    public double getGezahlteUmbuchungsgebuehr() {
+        return gezahlteUmbuchungsgebuehr;
+    }
+
+    public void setGezahlteUmbuchungsgebuehr(double gebuehr) {
+        this.gezahlteUmbuchungsgebuehr = gebuehr;
+    }
+
+    
 
     /**
      * Gibt eine textuelle Beschreibung der Buchung zurück.
@@ -197,6 +207,8 @@ public class Buchung implements Serializable {
                 this.gepaeckInformation.toString() +
                 "\n---------------------------------------------\n" +
                 "Damit beträgt die Buchungssumme: " +
-                String.format("%.2f", this.gezahlterPreis) + "€";
+                String.format("%.2f", this.gezahlterPreis) + "€" + 
+                "\nGezahlter Umbuchungsbetrag: " 
+                + String.format("%.2f", this.gezahlteUmbuchungsgebuehr) + "€";
     }
 }
--- Buchungssystem.java
+++ Buchungssystem.java
@@ -188,11 +188,10 @@ public class Buchungssystem implements Serializable {
             //nimmt sich explizit den Sitz aus dem gewählten Flug und belegt ihn (inklusive der Buchungsreferenz des Sitzplatzes).
             Sitzplatz neuerSitzplatz = flug.findeSitzplatz(sitzplatznummer);
             neuerSitzplatz.belegen();
-            neuerSitzplatz.setBuchung(buchung);
 
-            //berechnet die Umbuchungsgebuehr und addiert sie auf den gezahlten Preis
+            // berechnet und speichert den gezahlten Umbuchungsbetrag
             gebuehr = berechneUmbuchungsgebuehr(buchung, flug, neuerSitzplatz);
-            buchung.setGezahlterPreis(buchung.getGezahlterPreis() + gebuehr);
+            buchung.setGezahlteUmbuchungsgebuehr(gebuehr);
             
             //weist den neuen Sitzplatz der Buchung zu
             buchung.setSitzplatz(neuerSitzplatz);
@@ -202,6 +201,9 @@ public class Buchungssystem implements Serializable {
 
             //weist den (neuen oder bestehenden) Flug der Buchung zu
             buchung.setFlug(flug);
+
+            //aktualisiert den gezahlten Preis
+            buchung.aktualisiereGezahltenPreis();
 
             //aendert den Buchungsstatus
             buchung.setBuchungsstatus(Buchungsstatus.UMGEBUCHT);
@@ -327,6 +329,9 @@ public class Buchungssystem implements Serializable {
             preisNeuerFlug = flug.getBasispreis();
         }
 
+        //ergänze Koffergebühr für den neuen Flug 
+        preisNeuerFlug += buchung.getGepaeckinformation().berechneGepaeckgebuehr();
+
         //berechnet die Differenz aus altem und neuen Flug
         ticketDifferenz = preisNeuerFlug - preisAlterFlug;
 
@@ -395,8 +400,8 @@ public class Buchungssystem implements Serializable {
         throw new NoSuchElementException("Die Buchung ist nicht im System vorhanden.");
     }
 
-    if (buchung.getBuchungsstatus() == Buchungsstatus.STORNIERT) {
-        throw new IllegalStateException("Bei einer stornierten Buchung kann das Gepäck nicht geändert werden.");
+    if (buchung.getBuchungsstatus() != Buchungsstatus.AKTIV && buchung.getBuchungsstatus() != Buchungsstatus.UMGEBUCHT) {
+        throw new IllegalStateException("Das Gepäck kann bei dieser Buchung nicht mehr geändert werden.");
     }
 
     if (neueAnzahlKoffer < 0) {
@@ -405,7 +410,7 @@ public class Buchungssystem implements Serializable {
 
     double alterPreis = buchung.getGezahlterPreis();
 
-    buchung.setGepaeckinformation(neueAnzahlKoffer);
+    buchung.setAnzahlKoffer(neueAnzahlKoffer);
 
     double neuerPreis = buchung.getGezahlterPreis();
 
