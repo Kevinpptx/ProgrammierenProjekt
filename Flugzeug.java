f1658b5c87e5f852926acadf9018c8a1047b1d92
marcelmrxk <marcelmarxkors@gmail.com>
2026-08-08 22:03:37 +0200

    Tr f7 fix plane validation  in class flugzeug (#49)
    * fixed plane validation
    
    * fixed so that a plane can have no business rows

--- Flugzeug.java
+++ Flugzeug.java
@@ -57,13 +57,23 @@ public class Flugzeug implements Serializable{
             int sitzeProReihe,
             int businessReihen) {
 
-        // Überprüfen, ob das Flugzeug "Sinn ergibt"
+        // Validiert die übergebenen Attribute 
+
+        if (code == null || code.isBlank()) {
+            throw new IllegalArgumentException("Bitte geben Sie einen Code an.");
+        }
+
+        if (modell == null || modell.isBlank()) {
+            throw new IllegalArgumentException("Bitte geben Sie einen Modellnamen an.");
+        }
+
         if (anzahlReihen <= 0) {
             throw new IllegalArgumentException("Bitte geben Sie eine positive, ganzzahlige Anzahl an Sitzreihen von mindestens 1 ein!");
         }
 
-        if (anzahlReihen <= 0 || businessReihen > anzahlReihen) {
-            throw new IllegalArgumentException("Bitte geben Sie eine positive, ganzzahlige Anzahl an Business-Reihen von mindestens 1 ein. Die Anzahl der Business-Reihen darf zudem nicht größer als die Anzahl der verfügbaren Reihen sein.");
+        //Es ist erlaubt, dass ein Flugzeug keine Bisuness-Reihen hat (siehe Flugzeuge von "Billig-Airlines")
+        if (businessReihen < 0 || businessReihen > anzahlReihen) {
+            throw new IllegalArgumentException("Bitte geben Sie eine positive, ganzzahlige Anzahl an Business-Reihen von mindestens 0 ein. Die Anzahl der Business-Reihen darf zudem nicht größer als die Anzahl der verfügbaren Reihen sein.");
         }
 
         if (anzahlReihen > 100) {
@@ -72,6 +82,10 @@ public class Flugzeug implements Serializable{
 
         if (sitzeProReihe > 26) {
             throw new IllegalArgumentException("Die Anzahl der Sitzplätze pro Reihe darf nicht größer als 26 sein.");
+        }
+
+        if (sitzeProReihe < 1) {
+            throw new IllegalArgumentException("Die Anzahl der Sitze pro Reihe muss mindestens 1 betragen.");
         }
 
         this.code = code;
