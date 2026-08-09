b18a6161f8c50038bd8df1e958e641d411611616
Cedric Beckmann <cedric.beckmann@deutschebahn.com>
2026-08-09 19:07:17 +0200

    chore: add Javadoch

--- Flugzeug.java
+++ Flugzeug.java
@@ -159,10 +159,20 @@ public class Flugzeug implements Serializable{
         return this.sitzplaetzeVorlage;
     }
 
+    /**
+     * Gibt die Anzahl der Economy Sitzplätze des Flugzeugs zurück.
+     *
+     * @return anzahl Sitzplätze in der Economy Klasse
+     */
     public int getAnzahlEconomy() {
         return anzahlEconomy;
     }
 
+    /**
+     * Gibt die Anzahl der Business Sitzplätze des Flugzeugs zurück.
+     *
+     * @return anzahl Sitzplätze in der Business Klasse
+     */
     public int getAnzahlBusiness() {
         return anzahlBusiness;
     }
