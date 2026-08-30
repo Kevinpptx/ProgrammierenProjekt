8e3e98ea2e0cf8ace52c413cec5e46aa06f953de
cedbe6 <beckmann.cedric@icloud.com>
2026-08-30 22:44:29 +0200

    docs: update Anwendungsdatemn documentation

--- avigator/verwaltung/Anwendungsdaten.java
+++ avigator/verwaltung/Anwendungsdaten.java
@@ -3,6 +3,11 @@ package avigator.verwaltung;
 import java.io.Serial;
 import java.io.Serializable;
 
+/**
+ * Bündelt die dauerhaft zu speichernden Verwaltungs- und Buchungsdaten der Anwendung.
+ *
+ * @author Cedric Beckmann
+ */
 public class Anwendungsdaten implements Serializable {
 
     /**
@@ -11,21 +16,40 @@ public class Anwendungsdaten implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
+    /**
+     * Verwaltungssystem für Fluggesellschaften, Flugzeuge, Flughäfen und Flüge.
+     */
     private final Verwaltungssystem verwaltungssystem;
 
+    /**
+     * Buchungssystem für Passagiere und Buchungen.
+     */
     private final Buchungssystem buchungssystem;
 
+    /**
+     * Erstellt leere Anwendungsdaten mit einem neuen Verwaltungs- und Buchungssystem.
+     */
     public Anwendungsdaten() {
 
         this.verwaltungssystem = new Verwaltungssystem();
         this.buchungssystem = new Buchungssystem();
     }
 
+    /**
+     * Gibt das Verwaltungssystem zurück.
+     *
+     * @return das Verwaltungssystem der Anwendung
+     */
     public Verwaltungssystem getVerwaltungssystem() {
 
         return this.verwaltungssystem;
     }
 
+    /**
+     * Gibt das Buchungssystem zurück.
+     *
+     * @return das Buchungssystem der Anwendung
+     */
     public Buchungssystem getBuchungssystem() {
 
         return this.buchungssystem;
