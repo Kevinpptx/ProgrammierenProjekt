0afc97bbc912a189dc56b133ecc4ef8d3dcca5c9
cedbe6 <beckmann.cedric@icloud.com>
2026-08-30 22:41:05 +0200

    docs: update Sitzklasse documentation

--- avigator/modell/Sitzklasse.java
+++ avigator/modell/Sitzklasse.java
@@ -1,6 +1,17 @@
 package avigator.modell;
 
+/**
+ * Gibt die Reiseklasse eines Sitzplatzes an.
+ */
 public enum Sitzklasse {
+
+    /**
+     * Kennzeichnet einen Sitzplatz der Economy-Klasse.
+     */
     ECONOMY,
+
+    /**
+     * Kennzeichnet einen Sitzplatz der Business-Klasse.
+     */
     BUSINESS
 }
